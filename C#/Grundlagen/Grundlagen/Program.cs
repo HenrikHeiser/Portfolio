680a139a19758d2fe738b0ac3e9f7293fd914328
Henrik Heiser <122433513+HenrikHeiser@users.noreply.github.com>
2023-04-06 13:32:44 +0200

    c# basics

---
+++ C#/Grundlagen/Grundlagen/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grundlagen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Schreibe einen Taschenrechner der zwei Zahlen und eine Rechenmethode (+, -, x, :) und diese Rechnung
+            //dann ausrechnet
+            Console.WriteLine("Wähle eine Rechenart: \n [1] + \n [2] -");
+            Console.ReadKey();  
+
+        }
+    }
+}
